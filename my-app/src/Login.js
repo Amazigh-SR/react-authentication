54a7c180e6ce57206b512e375926b7e5649e91e0
Amazigh-SR <s_Red009@hotmail.com>
2021-05-14 00:43:50 -0400

    Create Login component

---
+++ my-app/src/Login.js
@@ -0,0 +1,17 @@
+const Login = function () {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <label>
+        Username:
+        <input type="text" placeholder="e.g. James" />
+      </label>
+      <label>
+        Password:
+        <input type="text" />
+      </label>
+    </div>
+  );
+};
+
+export default Login;
