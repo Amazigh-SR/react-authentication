e1874271daca84329d09cf4af784f5c7c3124307
Amazigh-SR <s_Red009@hotmail.com>
2021-05-14 00:44:01 -0400

    Create Signup component

---
+++ my-app/src/Signup.js
@@ -0,0 +1,17 @@
+const Signup = function () {
+  return (
+    <div>
+      <h2>Signup Page</h2>
+      <label>
+        Username:
+        <input type="text" placeholder="e.g. James" />
+      </label>
+      <label>
+        Password:
+        <input type="text" />
+      </label>
+    </div>
+  );
+};
+
+export default Signup;
