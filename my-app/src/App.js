3c97f556e07cdd03f4095b31a6cfbe819b1427c9
Amazigh-SR <s_Red009@hotmail.com>
2021-05-14 00:41:52 -0400

    Create App component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Authentication from "./Authentication";
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="App">
+      <h1>Challenge - React Authentication Page</h1>
+      <Authentication />
     </div>
   );
 }
